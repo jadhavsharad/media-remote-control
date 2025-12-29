0568851a90c15bc3ec305b52f336c7ab638bfb6c
Sharad Jadhav <jadhavsharad290@gmail.com>
2025-12-30 00:57:57 +0530

    ADDED PARING ON START AND STATE MANAGEMENT

--- background.js
+++ background.js
@@ -1,11 +1,19 @@
 
 let webSocket = null;
 
+const pairCode = Math.random().toString(36).slice(2, 8).toUpperCase();
+console.log("🔑 Pair code:", pairCode);
+
 function connect() {
   webSocket = new WebSocket('ws://localhost:3000');
 
-  webSocket.onopen = (event) => {
-    console.log('websocket open');
+  webSocket.onopen = () => {
+    webSocket.send(
+      JSON.stringify({
+        type: "PAIR",
+        pairCode
+      })
+    );
   };
 
   webSocket.onmessage = (event) => {
@@ -27,6 +35,20 @@ function disconnect() {
 }
 
 connect();
+
+chrome.runtime.onMessage.addListener((msg) => {
+  // state coming from content script
+  console.log("Background received message: ", msg);
+  if (msg.type === "STATE_UPDATE") {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify(msg));
+    }
+    return;
+  }
+
+  forwardToTab(msg);
+});
+
 
 const forwardToTab = (msg) => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
--- content.js
+++ content.js
@@ -1,6 +1,22 @@
+const video = document.querySelector("video");
+if (video) {
+  attachVideo(video);
+}
+
+function sendState(video) {
+  chrome.runtime.sendMessage({
+    type: "STATE_UPDATE",
+    state: video.paused ? "PAUSED" : "PLAYING"
+  });
+}
+
+function attachVideo(video) {
+  video.addEventListener("play", () => sendState(video));
+  video.addEventListener("pause", () => sendState(video));
+}
+
 chrome.runtime.onMessage.addListener((msg) => {
   if (msg.action === "TOGGLE_PLAYBACK") {
-    const video = document.querySelector("video");
     if (!video) return;
     video.paused ? video.play() : video.pause();
   }
